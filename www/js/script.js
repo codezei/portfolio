9c0edaa1066eb06a20194f08cf3218bff62af0e7
evgenij kachur <mwbox89@gmail.com>
2020-02-18 00:02:26 +0200

    page portfolio. done filters functional

--- www/js/script.js
+++ www/js/script.js
@@ -1,5 +1,12 @@
 window.addEventListener('DOMContentLoaded', ()=>{
+    let filters = document.querySelector('.filters');
+    let filterBtns = document.querySelectorAll('.filter-btn');
+    let filterItems = document.querySelectorAll('.filter-item');
     document.addEventListener('click', toggleArticles);
+    filters.addEventListener('click', (e)=>{
+        toggleFilterBtn(e.target, filterBtns);
+        filteringItems(filterItems);
+    })
 })
 
 function toggleArticles (e) {
@@ -14,5 +21,37 @@ function toggleArticles (e) {
         let activeArticle = document.querySelector('article.active');
         activeArticle.classList.remove('active');
         header.classList.remove('active');
+    }
+}
+
+
+function toggleFilterBtn (target, arrBtns) {
+
+    arrBtns.forEach((elem)=>{
+        if (elem === target) {
+            elem.classList.add('active');
+        } else {
+            elem.classList.remove('active');
+        }
+    });
+}
+function filteringItems (items) {
+    let activeBtn = document.querySelector('.filter-btn.active');
+    let valueBtn = activeBtn.dataset.target;
+
+    items.forEach(elem=>{
+        if (elem.dataset.category !== valueBtn) {
+            elem.classList.add('hide');
+
+        } 
+        else {
+            elem.classList.remove('hide');
+        }
+    })
+    if (valueBtn === 'all') {
+        console.log('hello')
+        items.forEach(elem=>{
+            elem.classList.remove('hide');
+        })
     }
 }
